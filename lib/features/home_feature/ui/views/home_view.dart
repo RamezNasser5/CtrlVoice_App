a7f02ebae2940ac542cf495374e95dabf2b88205
Ramez Nasser <rameznasser55@gmail.com>
2024-06-20 17:26:26 +0300

    Refactor HomeView and extract HomeComponents
    Refactored the HomeView to simplify the UI structure by extracting
    the components into a separate HomeComponents widget. This change
    improves code readability and maintainability. Additionally, added
    a gradient background to the HomeView for a better visual appearance.

--- lib/features/home_feature/ui/views/home_view.dart
+++ lib/features/home_feature/ui/views/home_view.dart
@@ -1,8 +1,5 @@
-import 'package:ctrlvoice/features/home_feature/logic/bloc/bloc/speech_to_text_bloc.dart';
-import 'package:ctrlvoice/features/home_feature/logic/cubits/assistant_ai/assistant_ai_cubit.dart';
-import 'package:ctrlvoice/features/home_feature/logic/cubits/text_to_speech/text_to_speech_cubit.dart';
+import 'package:ctrlvoice/features/home_feature/ui/widgets/home_components.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -10,62 +7,92 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            MaterialButton(
-              onPressed: () {
-                BlocProvider.of<SpeechToTextBloc>(context)
-                    .add(SpeechToTextStartEvent());
-              },
-              child: const Text('Listen'),
-            ),
-            MaterialButton(
-              onPressed: () {
-                BlocProvider.of<SpeechToTextBloc>(context)
-                    .add(SpeechToTextEndEvent());
-              },
-              child: const Text('Stop'),
-            ),
-            BlocBuilder<SpeechToTextBloc, SpeechToTextState>(
-              builder: (context, state) {
-                if (state is SpeechToTextStartLoading ||
-                    state is SpeechToTextEndLoading) {
-                  return const CircularProgressIndicator();
-                } else if (state is SpeechToTextStartSuccess) {
-                  return Text(state.message);
-                } else if (state is SpeechToTextStartFailure) {
-                  return Text('Error: ${state.message}');
-                } else if (state is SpeechToTextEndSuccess) {
-                  BlocProvider.of<AssistantAiCubit>(context)
-                      .processPrompt(state.message);
-                  return Text('Final Result: ${state.message}');
-                } else if (state is SpeechToTextEndFailure) {
-                  return Text('Error: ${state.message}');
-                } else {
-                  return const Text('Press the button to start listening');
-                }
-              },
-            ),
-            BlocBuilder<AssistantAiCubit, AssistantAiState>(
-              builder: (context, state) {
-                if (state is AssistantAiLoading) {
-                  return const CircularProgressIndicator();
-                } else if (state is AssistantAiSuccess) {
-                  BlocProvider.of<TextToSpeechCubit>(context)
-                      .speakText(state.message);
-                  return Text(state.message);
-                } else if (state is AssistantAiFailure) {
-                  return Text(state.message);
-                } else {
-                  return const Text('wait for prompt');
-                }
-              },
-            ),
+        body: Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          colors: [
+            Color(0xff4093CE),
+            Color(0xff9BCEF3),
           ],
         ),
       ),
-    );
+      child: const HomeComponents(),
+    ));
   }
 }
+
+
+
+// import 'package:ctrlvoice/features/home_feature/logic/bloc/bloc/speech_to_text_bloc.dart';
+// import 'package:ctrlvoice/features/home_feature/logic/cubits/assistant_ai/assistant_ai_cubit.dart';
+// import 'package:ctrlvoice/features/home_feature/logic/cubits/text_to_speech/text_to_speech_cubit.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+
+// class HomeView extends StatelessWidget {
+//   const HomeView({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             MaterialButton(
+//               onPressed: () {
+//                 BlocProvider.of<SpeechToTextBloc>(context)
+//                     .add(SpeechToTextStartEvent());
+//               },
+//               child: const Text('Listen'),
+//             ),
+//             MaterialButton(
+//               onPressed: () {
+//                 BlocProvider.of<SpeechToTextBloc>(context)
+//                     .add(SpeechToTextEndEvent());
+//               },
+//               child: const Text('Stop'),
+//             ),
+//             BlocBuilder<SpeechToTextBloc, SpeechToTextState>(
+//               builder: (context, state) {
+//                 if (state is SpeechToTextStartLoading ||
+//                     state is SpeechToTextEndLoading) {
+//                   return const CircularProgressIndicator();
+//                 } else if (state is SpeechToTextStartSuccess) {
+//                   return Text(state.message);
+//                 } else if (state is SpeechToTextStartFailure) {
+//                   return Text('Error: ${state.message}');
+//                 } else if (state is SpeechToTextEndSuccess) {
+//                   BlocProvider.of<AssistantAiCubit>(context)
+//                       .processPrompt(state.message);
+//                   return Text('Final Result: ${state.message}');
+//                 } else if (state is SpeechToTextEndFailure) {
+//                   return Text('Error: ${state.message}');
+//                 } else {
+//                   return const Text('Press the button to start listening');
+//                 }
+//               },
+//             ),
+//             BlocBuilder<AssistantAiCubit, AssistantAiState>(
+//               builder: (context, state) {
+//                 if (state is AssistantAiLoading) {
+//                   return const CircularProgressIndicator();
+//                 } else if (state is AssistantAiSuccess) {
+//                   BlocProvider.of<TextToSpeechCubit>(context)
+//                       .speakText(state.message);
+//                   return Text(state.message);
+//                 } else if (state is AssistantAiFailure) {
+//                   return Text(state.message);
+//                 } else {
+//                   return const Text('wait for prompt');
+//                 }
+//               },
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
---
+++ lib/features/home_feature/ui/widgets/home_components.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomeComponents extends StatelessWidget {
+  const HomeComponents({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        Text('Home View'),
+      ],
+    );
+  }
+}
