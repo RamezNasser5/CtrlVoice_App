afdb68ec11d807c90963dfd35aa07c9c52e53851
Ramez Nasser <rameznasser55@gmail.com>
2024-06-25 09:51:46 +0300

    Refactor AI prompt processing to use repository pattern
    Introduced HomeRepo and HomeRepoImpl to handle AI prompt processing.
    Refactored AssistantAiCubit to utilize HomeRepoImpl for fetching response
    data. This change improves code modularity and separation of concerns by
    delegating data fetching responsibilities to the repository layer.

---
+++ lib/features/home_feature/data/repo/home_repo_impl.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+import 'package:ctrlvoice/core/utils/consts.dart';
+import 'package:ctrlvoice/features/home_feature/data/repo/home_repo.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
+
+class HomeRepoImpl implements HomeRepo {
+  @override
+  Future<String> getResponseData(String prompt) async {
+    final apiKey = dotenv.env['API_KEY'];
+    if (apiKey == null) {
+      debugPrint('No \$API_KEY environment variable');
+      exit(1);
+    }
+    final model = GenerativeModel(model: 'gemini-1.5-flash', apiKey: apiKey);
+    final content = [Content.text(prompt + based)];
+    final response = await model.generateContent(content);
+    debugPrint(response.text);
+    return response.text!;
+  }
+}
